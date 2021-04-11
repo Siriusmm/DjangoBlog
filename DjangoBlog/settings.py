a195c885399163458609365eaef3053cda9fcd51
liangyin <247708244@qq.com>
2021-04-11 12:04:30 +0800

    db

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoblog',
         'USER': 'root',
-        'PASSWORD': 'mysqlroot',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
